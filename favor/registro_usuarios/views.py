482d333a5e9b99c5520258b312fb986982cf85ac
UserStatic <userstatic@userstatic-Lenovo-Z40-70.(none)>
2014-11-13 13:42:27 -0500

    Commit 6

--- favor/registro_usuarios/views.py
+++ favor/registro_usuarios/views.py
@@ -2,18 +2,60 @@ from django.shortcuts import render_to_response, render
 from django.template.context import RequestContext
 from django.http import HttpResponseRedirect
 # Usuario
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 # Formularios
-from forms import FormRegistrarUsuario,FormInciarSesion
+from forms import FormRegistrarUsuario, FormInciarSesion
 #Modelos
 from models import *
 # Create your views here.
+
+
 def home(request):
-	template='inicio.html' 
+	template='inicio.html'
 	form_registrar_usuario = FormRegistrarUsuario()
 	form_iniciar_sesion = FormInciarSesion()
-	print "hola mundp"
 	return render_to_response(template,{"form_registrar_usuario":form_registrar_usuario,"form_iniciar_sesion":form_iniciar_sesion},context_instance=RequestContext(request))
+
+
+def iniciarSesion(request):
+    if not request.user.is_authenticated():
+        if request.method == "POST":
+            iniciar_sesion = FormInciarSesion(request.POST)
+            if iniciar_sesion.is_valid():
+                email = iniciar_sesion.cleaned_data['iniciar_input_email']
+                clave = iniciar_sesion.cleaned_data['iniciar_input_password']
+                acceso = authenticate(username=email,password=clave)
+                if acceso is not None:
+                    if acceso.is_active:
+                        login(request,acceso)
+                        return HttpResponseRedirect('/principal')
+                    else:
+                        iniciar_sesion = FormInciarSesion(request.POST)
+                        return render_to_response('ingresoSesion.html',{'form_iniciar_sesion':iniciar_sesion,'error':'Su cuenta ha sido desactivada,por violar los derechos de uso'},context_instance = RequestContext(request))
+                else:
+                    iniciar_sesion = FormInciarSesion(request.POST)
+                    return render_to_response('ingresoSesion.html',{'form_iniciar_sesion':iniciar_sesion,'error':'Por favor Ingrese Correctamente su usuario o password'},context_instance=RequestContext(request))
+            else:
+                template = "ingresoSesion.html"
+                iniciar_sesion = FormInciarSesion(request.POST)
+                return render_to_response(template,{'form_iniciar_sesion':iniciar_sesion},context_instance=RequestContext(request))
+        else:
+            template = "ingresoSesion.html"
+            iniciar_sesion = FormInciarSesion(request.POST)
+            return render_to_response(template,{'form_iniciar_sesion':iniciar_sesion},context_instance=RequestContext(request))
+    else:
+        return HttpResponseRedirect("/principal")
+
+
+def principal(request):
+    template = "principal.html"
+    return render_to_response(template,context_instance=RequestContext(request))
+
+
+@login_required(login_url='/login')
+def cerrarSesion(request):
+    logout(request)
+    return  HttpResponseRedirect('/')
