eb3e577efd87d062a7c8e21a8f54eafd9181f943
sleonr0792 <sleonr0792>
2014-11-26 15:48:23 -0500

    hola a todos

--- favor/favor/urls.py
+++ favor/favor/urls.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django.conf.urls import patterns, include, url
 #Settings
 from django.conf import settings
@@ -18,33 +18,9 @@ urlpatterns = patterns('',
     url(r'^signup/$', 'registro_usuarios.views.registrarUsuario', name='signup'),
     url(r'^principal/$', 'registro_usuarios.views.principal', name='principal'),
     url(r'^logout/$', 'registro_usuarios.views.cerrarSesion', name='logout'),
+    url(r'^principal/preferencias/$', 'registro_usuarios.views.preferencias', name='preferencias'),
      url(r'^media/(?P<path>.*)$','django.views.static.serve',
 		{'document_root':settings.MEDIA_ROOT,} 
 		),
     url(r'^admin/', include(admin.site.urls)),
 )
-=======
-from django.conf.urls import patterns, include, url
-#Settings
-from django.conf import settings
-#finSetting
-from django.contrib import admin
-
-
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'favor.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^$', 'registro_usuarios.views.home', name='home'),
-    url(r'^$', 'registro_usuarios.views.home', name='home'),
-    url(r'^login/$', 'registro_usuarios.views.iniciarSesion', name='login'),
-    url(r'^principal/$', 'registro_usuarios.views.principal', name='principal'),
-    url(r'^logout/$', 'registro_usuarios.views.cerrarSesion', name='logout'),
-     url(r'^media/(?P<path>.*)$','django.views.static.serve',
-		{'document_root':settings.MEDIA_ROOT,}
-		),
-    url(r'^admin/', include(admin.site.urls)),
-)
->>>>>>> 829ebd0583aa9a2a1027ad17ea9035e47ea645e6
--- favor/registro_usuarios/views.py
+++ favor/registro_usuarios/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render_to_response, render
 from django.template.context import RequestContext
 from django.http import HttpResponseRedirect
@@ -70,66 +69,24 @@ def principal(request):
 def cerrarSesion(request):
     logout(request)
     return  HttpResponseRedirect('/')
-=======
-from django.shortcuts import render_to_response, render
-from django.template.context import RequestContext
-from django.http import HttpResponseRedirect
-# Usuario
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib import auth
-# Formularios
-from forms import FormRegistrarUsuario, FormInciarSesion
-#Modelos
-from models import *
-# Create your views here.
+
+Programa
 
 
-def home(request):
-    template='inicio.html'
-    form_registrar_usuario = FormRegistrarUsuario()
-    form_iniciar_sesion = FormInciarSesion()
-    return render_to_response(template,{"form_registrar_usuario":form_registrar_usuario,"form_iniciar_sesion":form_iniciar_sesion},context_instance=RequestContext(request))
 
 
-def iniciarSesion(request):
-    if not request.user.is_authenticated():
-        if request.method == "POST":
-            iniciar_sesion = FormInciarSesion(request.POST)
-            if iniciar_sesion.is_valid():
-                email = iniciar_sesion.cleaned_data['iniciar_input_email']
-                clave = iniciar_sesion.cleaned_data['iniciar_input_password']
-                acceso = authenticate(username=email,password=clave)
-                if acceso is not None:
-                    if acceso.is_active:
-                        login(request,acceso)
-                        return HttpResponseRedirect('/principal')
-                    else:
-                        iniciar_sesion = FormInciarSesion(request.POST)
-                        return render_to_response('ingresoSesion.html',{'form_iniciar_sesion':iniciar_sesion,'error':'Su cuenta ha sido desactivada,por violar los derechos de uso'},context_instance = RequestContext(request))
-                else:
-                    iniciar_sesion = FormInciarSesion(request.POST)
-                    return render_to_response('ingresoSesion.html',{'form_iniciar_sesion':iniciar_sesion,'error':'Por favor Ingrese Correctamente su usuario o password'},context_instance=RequestContext(request))
-            else:
-                template = "ingresoSesion.html"
-                iniciar_sesion = FormInciarSesion(request.POST)
-                return render_to_response(template,{'form_iniciar_sesion':iniciar_sesion},context_instance=RequestContext(request))
-        else:
-            template = "ingresoSesion.html"
-            iniciar_sesion = FormInciarSesion(request.POST)
-            return render_to_response(template,{'form_iniciar_sesion':iniciar_sesion},context_instance=RequestContext(request))
-    else:
-        return HttpResponseRedirect("/principal")
+def preferencias(request):
+    preferencias_todas = Programa.objects.all()
+    categoria_todas = Categoria.objects.all()
+    template = "inicio2.html"
+    return render_to_response(template, {"preferencia_dato":preferencias_todas , "categoria_dato":categoria_todas},context_instance=RequestContext(request))
 
 
-def principal(request):
-    template = "principal.html"
-    return render_to_response(template,context_instance=RequestContext(request))
+"""
+    def elegir_preferencias(request,valor_preferencia):
+        if valor_preferencia == "todos":
+            preferencias_todos = Programa.objects.all()
+    """    
 
 
-@login_required(login_url='/login')
-def cerrarSesion(request):
-    logout(request)
-    return  HttpResponseRedirect('/')
->>>>>>> 829ebd0583aa9a2a1027ad17ea9035e47ea645e6
+    
